208c1844bce43cb49389e8b26f8a3de98e46935f
Muhammad Zaid <mzaid.se@gmail.com>
2017-03-24 02:13:26 +0500

    Initial Commit

---
+++ src/main/java/com/app/config/audit/package-info.java
@@ -0,0 +1,4 @@
+/**
+ * Audit specific code.
+ */
+package com.app.config.audit;
---
+++ src/main/webapp/app/entities/entity.state.js
@@ -0,0 +1,16 @@
+(function() {
+    'use strict';
+
+    angular
+        .module('angularSpringApp1App')
+        .config(stateConfig);
+
+    stateConfig.$inject = ['$stateProvider'];
+
+    function stateConfig($stateProvider) {
+        $stateProvider.state('entity', {
+            abstract: true,
+            parent: 'app'
+        });
+    }
+})();
